18cda5b5bc147564cb54cc7379340136af6e9ede
Qiaosi Wang <wqs@uw.edu>
2018-01-30 23:54:38 -0800

    finished auto layout

---
+++ simplecalc-ios copy/simplecalc-ios/HistoryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryViewController.swift
+//  simplecalc-ios
+//
+//  Created by Qiaosi Wang on 1/30/18.
+//  Copyright © 2018 ischool-uw. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HistoryViewController : UIViewController{
+    
+}
